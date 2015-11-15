f2f0395bfdfed9cec7370f7d0391622f88b457fe
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-15 14:29:52 +0100

    exercise 3 widget formatting completed

---
+++ app/src/main/java/com/example/xiaolong/exercises/a3/a3_textformatting_color.java
@@ -0,0 +1,15 @@
+package com.example.xiaolong.exercises.a3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.xiaolong.exercises.R;
+
+public class a3_textformatting_color extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a3_textformatting_color);
+    }
+}
