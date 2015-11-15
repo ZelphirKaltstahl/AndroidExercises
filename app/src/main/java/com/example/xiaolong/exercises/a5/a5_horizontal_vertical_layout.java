69999b2b9f77841ca9a8e6c49e6f962a83e9fa7a
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-15 21:47:53 +0100

    layout of calculator changed

--- app/src/main/java/com/example/xiaolong/exercises/a5/a5_horizontal_vertical_layout.java
+++ app/src/main/java/com/example/xiaolong/exercises/a5/a5_horizontal_vertical_layout.java
@@ -1,15 +1,13 @@
 package com.example.xiaolong.exercises.a5;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.xiaolong.exercises.R;
-
-public class a5_calculator_activity extends Activity {
+public class a5_horizontal_vertical_layout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator);
+        setContentView(R.layout.activity_a5_horizontal_vertical_layout);
     }
 }
