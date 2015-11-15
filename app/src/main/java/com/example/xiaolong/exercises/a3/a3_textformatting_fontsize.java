339d54dda4f9eae4d87e3c655a8f395e9e9a5bf5
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-15 14:19:16 +0100

    fontsize widgets formatting completed

--- app/src/main/java/com/example/xiaolong/exercises/a3/a3_textformatting_fontsize.java
+++ app/src/main/java/com/example/xiaolong/exercises/a3/a3_textformatting_fontsize.java
@@ -1,9 +1,11 @@
 package com.example.xiaolong.exercises.a3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class a3_textformatting_fontsize extends AppCompatActivity {
+import com.example.xiaolong.exercises.R;
+
+public class a3_textformatting_fontsize extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
