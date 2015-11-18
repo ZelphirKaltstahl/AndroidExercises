6a81694dec6ce2a859372b5723ea1bbae9fd9183
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-18 18:36:29 +0100

    added descriptive labels to the buttons of exercises 5 and 6 in the main activity

--- app/src/main/java/com/example/xiaolong/exercises/a6/a6_listview_activity.java
+++ app/src/main/java/com/example/xiaolong/exercises/a6/a6_listview_activity.java
@@ -19,7 +19,6 @@ public class a6_listview_activity extends Activity {
 
     private ListView equations_listview;
     private ArrayAdapter<String> listview_adapter;
-    private ArrayList<String> list_of_equation_strings = new ArrayList<>();
     private Intent open_calculator_intent;
 
     @Override
@@ -29,7 +28,7 @@ public class a6_listview_activity extends Activity {
 
         equations_listview = (ListView) findViewById(R.id.a6_listview_listview);
         equations_listview.setAdapter(listview_adapter);
-        listview_adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list_of_equation_strings);
+        listview_adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
     }
 
     public void on_add(View view) {
@@ -43,10 +42,13 @@ public class a6_listview_activity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent intent_with_data) {
         if (resultCode == RESULT_OK) {
             if (intent_with_data == null) {
-                Log.d(a6_listview_activity.LISTVIEW_ACTIVITY_LOG_TAG, "intent didn't carry data");
+                Log.d(a6_listview_activity.LISTVIEW_ACTIVITY_LOG_TAG, "intent was null");
             } else {
+                Log.d(a6_listview_activity.LISTVIEW_ACTIVITY_LOG_TAG, "checking who responded");
                 if(requestCode == MyActivity.ENTER_EQUATION.ordinal()) {
+                    Log.d(a6_listview_activity.LISTVIEW_ACTIVITY_LOG_TAG, "calculator responded");
                     if(intent_with_data.hasExtra(a_calculator.EQUATION_EXTRA_STRING)) {
+                        Log.d(a6_listview_activity.LISTVIEW_ACTIVITY_LOG_TAG, "intent has required extra, now adding it to listview adapter");
                         listview_adapter.add(intent_with_data.getStringExtra(a_calculator.EQUATION_EXTRA_STRING));
                     }
                 }
