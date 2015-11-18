457525599434211be86998675d834e14604bd293
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-18 17:03:29 +0100

    added a button to the layout

--- app/src/main/java/com/example/xiaolong/exercises/a6/a6_listview_activity.java
+++ app/src/main/java/com/example/xiaolong/exercises/a6/a6_listview_activity.java
@@ -2,14 +2,21 @@ package com.example.xiaolong.exercises.a6;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.xiaolong.exercises.R;
 
 public class a6_listview_activity extends Activity {
 
+    private ListView equations_listview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a6_listview_activity);
+
+        equations_listview = (ListView) findViewById(R.id.a6_listview_listview);
     }
+
+
 }
