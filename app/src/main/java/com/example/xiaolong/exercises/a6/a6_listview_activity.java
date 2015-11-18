68b42fd157a2429df299b01301199d6f0e8d61f0
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-18 12:54:42 +0100

    activity for exercise 6 added

---
+++ app/src/main/java/com/example/xiaolong/exercises/a6/a6_listview_activity.java
@@ -0,0 +1,15 @@
+package com.example.xiaolong.exercises.a6;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.xiaolong.exercises.R;
+
+public class a6_listview_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a6_listview_activity);
+    }
+}
