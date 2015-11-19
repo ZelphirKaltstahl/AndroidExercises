7b4ef7507da3ee452eb60b4e40fc06203c879457
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-11-19 09:55:36 +0100

    activity for exercise7 created; string values created

---
+++ app/src/main/java/com/example/xiaolong/exercises/a7/a7_intents.java
@@ -0,0 +1,16 @@
+package com.example.xiaolong.exercises.a7;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xiaolong.exercises.R;
+
+public class a7_intents extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a7_intents);
+    }
+}
